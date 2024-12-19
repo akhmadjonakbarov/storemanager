008dccb7968d71511ae24029fd823a38b28662b3
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2024-12-19 05:40:48 +0500

    routes was updated

--- main.py
+++ main.py
@@ -12,33 +12,15 @@ app = FastAPI(
 )
 Base.metadata.create_all(bind=engine)
 
-# routes
 BASE_API_URL = "/api/v1"
-app.include_router(
-    user_routes.router, prefix=BASE_API_URL
-)
-app.include_router(
-    store_routes.router, prefix=BASE_API_URL
-)
-app.include_router(
-    item_routes.router, prefix=BASE_API_URL
-)
-app.include_router(
-    unit_routes.router, prefix=BASE_API_URL
-)
-app.include_router(
-    category_routes.router, prefix=BASE_API_URL
-)
-
-app.include_router(
-    document_routes.router, prefix=BASE_API_URL
-)
-app.include_router(
-    debt_routes.router, prefix=BASE_API_URL
-)
-app.include_router(
-    currency_type_routes.router, prefix=BASE_API_URL
-)
+app.include_router(user_routes.router, prefix=BASE_API_URL)
+app.include_router(store_routes.router, prefix=BASE_API_URL)
+app.include_router(item_routes.router, prefix=BASE_API_URL)
+app.include_router(unit_routes.router, prefix=BASE_API_URL)
+app.include_router(category_routes.router, prefix=BASE_API_URL)
+app.include_router(document_routes.router, prefix=BASE_API_URL)
+app.include_router(debt_routes.router, prefix=BASE_API_URL)
+app.include_router(currency_type_routes.router, prefix=BASE_API_URL)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
