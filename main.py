753b2b2fbdb413faf289fd20b4bd3caf7d863e98
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2024-12-20 02:48:25 +0500

    feat: add customer routes

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
 from apps.currency.routes import currency_type_routes
-from apps.product_manager.routes import item_routes, unit_routes, category_routes, document_routes, store_routes
+from apps.product_manager.routes import (
+    item_routes, unit_routes, category_routes,
+    document_routes, store_routes
+)
 from apps.user import routes as user_routes
 from apps.debt import routes as debt_routes
+from apps.customer import routes as customer_routes
 from config.database_config import engine
 from apps.base.models import Base
 
@@ -21,6 +25,7 @@ app.include_router(category_routes.router, prefix=BASE_API_URL)
 app.include_router(document_routes.router, prefix=BASE_API_URL)
 app.include_router(debt_routes.router, prefix=BASE_API_URL)
 app.include_router(currency_type_routes.router, prefix=BASE_API_URL)
+app.include_router(customer_routes.router, prefix=BASE_API_URL)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
