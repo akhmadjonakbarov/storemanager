8f373af1e5ccaf29e4c5c3110b1f4e84faee7a56
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2024-12-20 02:48:06 +0500

    feat: validation was added for phone_number and phone_number2

--- apps/customer/models.py
+++ apps/customer/models.py
@@ -1,12 +1,14 @@
 from apps.base.models import Base
 from sqlalchemy import Column, String, Boolean, Integer, ForeignKey, Date, DateTime, Float
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, validates
+import re
 
 
 class CustomerModel(Base):
     __tablename__ = 'customers'  # Specify the database table name
+
     full_name = Column(String(100), nullable=False)
-    phone_number = Column(String(15), nullable=False, unique=True)
+    phone_number = Column(String(15), nullable=False, unique=True, index=True)
     phone_number2 = Column(String(9), nullable=True, unique=True)
     address = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True)
@@ -17,6 +19,12 @@ class CustomerModel(Base):
 
     # Relationship to CustomerDebtModel
     customer_debts = relationship('CustomerDebtModel', back_populates='customer')
+
+    @validates('phone_number', 'phone_number2')
+    def validate_phone_number(self, key, value):
+        if value and not re.match(r'^\+?\d{9,15}$', value):
+            raise ValueError('Invalid phone number format. Expected format: 901234567.')
+        return value
 
 
 class CustomerDebtModel(Base):
