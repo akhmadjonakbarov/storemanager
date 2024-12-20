cd06699b206e1ef0fc3e7950f699d3f861fca9df
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2024-12-21 03:58:39 +0500

    new route was added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from apps.currency.routes import currency_type_routes
 from apps.product_manager.routes import (
     item_routes, unit_routes, category_routes,
-    document_routes, store_routes
+    document_routes, store_routes, doc_item_routes
 )
 from apps.user import routes as user_routes
 from apps.debt import routes as debt_routes
@@ -19,14 +19,16 @@ Base.metadata.create_all(bind=engine)
 
 BASE_API_URL = settings.API_V1_STR
 app.include_router(user_routes.router, prefix=BASE_API_URL)
+app.include_router(customer_routes.router, prefix=BASE_API_URL)
+app.include_router(document_routes.router, prefix=BASE_API_URL)
+
+app.include_router(doc_item_routes.router, prefix=BASE_API_URL)
 app.include_router(store_routes.router, prefix=BASE_API_URL)
 app.include_router(item_routes.router, prefix=BASE_API_URL)
 app.include_router(unit_routes.router, prefix=BASE_API_URL)
 app.include_router(category_routes.router, prefix=BASE_API_URL)
-app.include_router(document_routes.router, prefix=BASE_API_URL)
 app.include_router(debt_routes.router, prefix=BASE_API_URL)
 app.include_router(currency_type_routes.router, prefix=BASE_API_URL)
-app.include_router(customer_routes.router, prefix=BASE_API_URL)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
